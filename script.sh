2d9f0ef8c7fd4385a5e727e888fffffed29893c5
Nicolas <nicolas.frbezar@decathlon.com>
2023-09-21 19:01:26 +0200

    Create script
    Signed-off-by: Nicolas <nicolas.frbezar@decathlon.com>

---
+++ script.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+
+set -eo pipefail
+
+BRANCH=main
+
+for file in $(find helm -name '*.yml' -or -name '*.yaml'); 
+do 
+    # Name of the dependency
+    name=$(yq e 'select(.kind == "HelmRelease" and .spec.chart.spec.sourceRef.kind == "HelmRepository").spec.releaseName' $file)
+
+    # Read Helm dependencies
+    helm_repo_ref=$(yq e 'select(.kind == "HelmRelease" and .spec.chart.spec.sourceRef.kind == "HelmRepository").spec.chart.spec.sourceRef.name' $file)
+    chart=$(yq e 'select(.kind == "HelmRelease" and .spec.chart.spec.sourceRef.kind == "HelmRepository").spec.chart.spec.chart' $file)
+    version=$(yq e 'select(.kind == "HelmRelease" and .spec.chart.spec.sourceRef.kind == "HelmRepository").spec.chart.spec.version' $file)
+
+    repo_name=$(yq e 'select(.kind == "HelmRepository").metadata.name' $file)
+    repo_url=$(yq e 'select(.kind == "HelmRepository").spec.url' $file)
+
+    # Select latest release of Helm chart
+    helm repo add $repo_name $repo_url
+    helm search repo -r "\v$repo_name/$chart\v" -l -o yaml | yq e 'map(select(key==0)).0' > tmp.txt
+    current_version=$(yq e '.version' tmp.txt)
+    current_app_version=$(echo $helm_latest | yq e '.app_version' tmp.txt)
+    rm -rf tmp.txt
+
+    # Sanitize the repo name
+    sanitized_name=$(echo $repo_name | tr -d ' ' | tr '/' '-')
+
+    # Output
+    echo "Name: $name"
+    echo "Version in HelmRelease: $version"
+    echo "Current Version: $current_version"
+
+    # If there's a difference between the versions
+    if [ "$version" != "$current_version" ]; then
+        if [ ! $(git branch -r --list origin/update-helm-$sanitized_name-$current_version) ]; then
+            echo "There's a difference between the charts versions."
+            # Get new packages values
+            helm show values $repo_name/$chart --version $current_version > new_values.yaml
+            helm show values $repo_name/$chart --version $version > old_values.yaml
+            
+            # Perform a diff on the two files
+            diff_result=$(dyff between old_values.yaml new_values.yaml) || true
+
+            # Prepare PR
+            echo -e "A new chart update has been found!\n\nRecommended image version: **$current_app_version**\n\nPlease **take a deep look onto compatibility matrix** for this app, **ensure the version match the cluster** and **update your values** before merging.\n\n" > pr.txt
+            echo "$diff_result" > diff_result.txt
+            awk '{ printf "\t%s\n", $0 }' diff_result.txt >> pr.txt
+            pr_body=$(cat pr.txt)
+
+            # Delete the temporary files
+            rm old_values.yaml new_values.yaml diff_result.txt pr.txt
+            
+            # Replace the old version with the new version in the Chart.yaml file using sed
+            name=$name current_version=$current_version yq -i 'select(.kind == "HelmRelease" and .spec.releaseName == env(name)).spec.chart.spec.version = env(current_version)' $file
+            
+            # Cleaning old branch existing
+            if [ $(git branch -r --list "origin/update-helm-$sanitized_name-*") ]; then
+                echo "There is pending branch for $name"
+                for existing_branch in $(git branch -r --list "origin/update-helm-$sanitized_name-*"); 
+                do 
+                echo "Found outdated update branch which propose update to ${existing_branch##*-}, removing it"
+                [[ ${existing_branch##*-} != $current_version ]] && git push -d origin update-helm-$sanitized_name-${existing_branch##*-}
+                done
+            fi
+
+            # Create a new branch for this change
+            git checkout -b update-helm-$sanitized_name-$current_version
+            git add "$file"
+            git commit -m "Update $name version from $version to $current_version"
+            git push origin update-helm-$sanitized_name-$current_version
+
+            # Create a GitHub Pull Request
+            gh pr create --title "Update $name version from $version to $current_version" --body "$pr_body" --base main --head update-helm-$sanitized_name-$current_version || true
+            
+            git checkout $BRANCH
+        else
+            echo "Branch already exists. Checking out to the existing branch." || true
+        fi
+    #
+    else
+        echo "There's no difference between the versions."
+    fi
+done
